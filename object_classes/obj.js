d9d2ac94b70c9bd9ac8c6804747fb71d5d7a5196
ajaykumarguptaa <theincredibleknowledge@gmail.com>
2025-12-16 20:11:49 +0530

    Add initial implementation of Device class with properties and constructors

---
+++ object_classes/obj.js
@@ -0,0 +1,26 @@
+"use strict";
+//classe and objects 
+//class defination
+// constructor 
+// access modifiers { public,private,protected}
+// read only proprty
+// Optional property
+//Perametric Proerty
+// getter and setter method
+// statric member 
+// Abstract class and methods
+Object.defineProperty(exports, "__esModule", { value: true });
+class Device {
+    name;
+    price;
+    constructor() {
+        this.name = "ajay",
+            this.price = 12000;
+    }
+}
+let d1 = new Device();
+let d2 = new Device();
+console.table(d1);
+console.table(d2);
+//! constructor-------------
+//# sourceMappingURL=obj.js.map
---
+++ object_classes/obj.ts
@@ -0,0 +1,30 @@
+//classe and objects 
+//class defination
+// constructor 
+// access modifiers { public,private,protected}
+// read only proprty
+// Optional property
+//Perametric Proerty
+// getter and setter method
+// statric member 
+// Abstract class and methods
+
+
+class Device{
+  name:string;
+  price:number;
+
+  constructor(){
+    this.name="ajay",
+    this.price=12000
+  }
+}
+
+let d1=new Device();
+let d2=new Device();
+
+console.table(d1);
+console.table(d2);
+
+//! constructor-------------
+
